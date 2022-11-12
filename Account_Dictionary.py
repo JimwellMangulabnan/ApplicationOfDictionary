9efd82002b93fe981441ca141b87026a28ed5d63
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-11-13 01:36:14 +0800

    Sample Account

--- Account_Dictionary.py
+++ Account_Dictionary.py
@@ -2,3 +2,44 @@ print("*********  PROGRAMMED BY  ********")
 print("***** JIMWELL L. MANGULABNAN *****")
 print("********** BSCOE 2-2 *************")
 print()
+
+# Display a menu 
+print()
+print("#######################")
+print("#        MENU         #")
+print("#---------------------#")
+print("#   1 >> Add an item  #")	
+print("#   2 >> Search       #")
+print("#   3 >> Exit         #")	 
+print("#######################")
+print()
+#listed Accounts
+accounts = {
+    "harry Potter" : {
+        "Age": 19,
+        "Status" : "Single",
+        "Gender" : "Male"
+    },
+    "enola Homes" : {
+        "Age": 16,
+        "Status" : "Single",
+        "Gender" : "Female"
+    },
+    "Marian Rivera" : {
+        "Age": 30,
+        "Status" : "Married",
+        "Gender" : "Female"
+    },
+    "Naruto Uzumaki " : {
+        "Age": 47,
+        "Status" : "widow",
+        "Gender" : "Male"
+    }
+}
+#ask the user
+choice = int(input("Enter a number that you would like to do: "))
+#if option 1 = add item
+if choice == 1:
+    
+#if option 2 = search
+#if option 3 = exit
