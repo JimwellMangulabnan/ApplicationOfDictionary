c835f91143a16ffaea7bd2c30e7b8d4dcea9d1f0
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-11-13 03:20:23 +0800

    Done

--- Account_Dictionary.py
+++ Account_Dictionary.py
@@ -52,51 +52,63 @@ while True:
     choice = int(input("Enter a number that you would like to do: "))
     #if option 1 = add item
     if choice == 1:
-        print("\n\t\t|  Add an item |\n")
-        name = input("Enter the fullname (first name, surname): ")
-        age = int(input("Enter his/her age: "))
-        status = input("Enter the status of the person: ")
-        gender = input("Enter the gender: ")
-        money = input("please Enter your money savings: (with $ sign)")
-        accounts[name] = {
-            "Age": age,
-            "Status" : status,
-            "Gender" : gender,
-            "Saving" : money
-        }
-        print("Succesfully added!!!\n")
-        print("this is the accounts have been updated: ", accounts)
-        print_menu()
+        try: 
+            print("\n\t\t|  Add an item |\n")
+            name = input("Enter the fullname (first name, surname): ")
+            age = int(input("Enter his/her age: "))
+            status = input("Enter the status of the person: ")
+            gender = input("Enter the gender: ")
+            money = input("please Enter your money savings: (Please put $ sign) ")
+        except ValueError:
+            print("\nInvalid input")
+        else:
+            accounts[name] = {
+                "Age": age,
+                "Status" : status,
+                "Gender" : gender,
+                "Saving" : money
+            }
+            print("Succesfully added!!!\n")
+            print("this is the accounts have been updated: ", accounts)
+            print_menu()
         
     #if option 2 = search
     elif choice == 2:
         print("\n\t\t|  Search |\n")
-        fullname = input("Please Enter the Fullname you would like to search or type ALL to see all: ")
-        if fullname == "ALL":
-            for key, value in accounts.items():
-                print ("Name: " + key)
-                print ("info: " + str(value) + "\n")
-        elif fullname in accounts:
-            print ("Name: " + fullname)
-            print ("info: " + str(accounts[fullname]))
+        try:
+            fullname = input("Please Enter the Fullname you would like to search or type ALL to see all: ")
+        except ValueError:
+            print("\nInvalid input")
         else:
-            print("I'm Sorry, invalid input!!!")
-        print_menu()
+            if fullname == "ALL":
+                for key, value in accounts.items():
+                    print ("Name: " + key)
+                    print ("info: " + str(value) + "\n")
+            elif fullname in accounts:
+                print ("Name: " + fullname)
+                print ("info: " + str(accounts[fullname]))
+            else:
+                print("I'm Sorry, invalid input!!!")
+            print_menu()
 
             
     #if option 3 = exit
     elif choice ==3:
         print("\n\t\t|  Exit |\n")
-        decision = int(input("Would you like to end this program? please Enter 1 if no and 2 if yes: "))
-        if decision == 1:
-            print_menu()
+        try:
+            decision = int(input("Would you like to end this program? please Enter 1 if no and 2 if yes: "))
+        except ValueError:
+            print("\nInvalid input")
         else:
-            print("""
-        ===================================
-        |                                 |
-        |           Thank You             |
-        |    For Using The Program!!!     |
-        |                                 |
-        ===================================  
-        """)
-            break  
+            if decision == 1:
+                print_menu()
+            else:
+                print("""
+            ===================================
+            |                                 |
+            |           Thank You             |
+            |    For Using The Program!!!     |
+            |                                 |
+            ===================================  
+            """)
+                break  
